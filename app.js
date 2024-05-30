e645a587ad2078e1845eb9498798f64d4268750d
xunxuno <sintraxlch@gmail.com>
2024-05-29 20:44:39 -0600

    api

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const routes = require('./routes/routes');
 const dotenv = require('dotenv');
+const usuariosController = require('./controllers/usuariosController');
 
 //Configura DotEnv
 dotenv.config();
@@ -12,8 +13,27 @@ app.use(express.json());
 // Rutas generales
 app.use('/', routes);
 
+// Ruta de prueba
+app.get('/', (req, res) => {
+    res.send('¡Hola, mundo!');
+});
+
+// Ruta para registrar usuario
+app.post('/api/registrar', usuariosController.registrarUsuario);
+
+// Ruta para login de usuario
+app.post('/api/login', usuariosController.loginUsuario);
+
+// Ruta de ejemplo
+app.get('/api/saludo', (req, res) => {
+    res.json({ mensaje: 'Hola desde la API' });
+});
+
+
+
+
 // Puerto en el que escucha el servidor
-const PORT = process.env.PORT || 3002;
-app.listen(PORT, () => {
+const port = process.env.PORT || 3002;
+app.listen(port, () => {
     console.log(`Servidor iniciado en http://localhost:${port}/`);
 });
